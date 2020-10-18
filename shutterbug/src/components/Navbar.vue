bda85e92101b2d97e113239422ea080a4034b1b0
KyleAHinton <kyleahinton1@gmail.com>
2020-10-18 11:25:32 -0400

    Addition of Vuetify and basic Navbar

---
+++ shutterbug/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <v-toolbar>
+            <v-toolbar-title class="text-uppercase white--text">
+                <span>Shutterbug</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn color="grey">
+                <span>Sign Out</span>
+            </v-btn>
+        </v-toolbar>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
