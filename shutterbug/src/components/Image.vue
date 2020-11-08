fdd025c50690c54b0e832385e775403612776ae9
KyleAHinton <kyleahinton1@gmail.com>
2020-11-07 23:42:19 -0500

    Support for image modification page and image searching.

---
+++ shutterbug/src/components/Image.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="image-box">
+    <img :src="String(this.images[Number(this.$route.params.id)])">
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+      images: localStorage.getItem("images").split(",")
+  }),
+};
+</script>
+
+<style scoped>
+.image-box {
+  width: 60%;
+  position: absolute;
+  transform: translateY(50%);
+  transform: translateX(50%);
+  margin-top: 50px;
+}
+</style>
