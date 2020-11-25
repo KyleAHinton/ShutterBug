c7ed8fc43c8f14448061653d280df169ed540b69
rebeljedi999 <38987640+rebeljedi999@users.noreply.github.com>
2020-11-24 22:13:15 -0500

    recommit of edit backend
    messed up previous commit

--- backend/portfolio/views.py
+++ backend/portfolio/views.py
@@ -1,15 +1,25 @@
+import string
+import random
+from io import BytesIO
+from urllib.parse import urljoin
+
 from django.shortcuts import render
 
-from django.http.response import JsonResponse
+from django.http.response import JsonResponse, Http404, HttpResponse
 from rest_framework.parsers import JSONParser 
 from rest_framework import status
+import portfolio
  
 from portfolio.models import Portfolio
 from portfolio.serializers import PortfolioSerializer
 from portfolio.models import Image
 from portfolio.serializers import ImageSerializer
 from rest_framework.decorators import api_view
-
+import requests
+from django.conf import settings
+import os
+from PIL import Image as PImage
+import json
 
 @api_view(['GET', 'POST', 'DELETE'])
 def PortfolioViewSet(request):
@@ -28,14 +38,14 @@ def PortfolioViewSet(request):
     # POST request handler
     elif request.method == 'POST':
         portfolio_data = JSONParser().parse(request)
-        if 'photo' in portfolio_data.keys():
-            port_id = portfolio_data['photo']
+        if 'id' in portfolio_data.keys():
+            port_id = portfolio_data['id']
             #port_usr = portfolio_data['user']
         else:
             port_id = None
         # check if id is provided, then run update
         if port_id is not None:
-            portfolio_f = Portfolio.objects.all().filter(photo=port_id)[0]
+            portfolio_f = Portfolio.objects.all().filter(id=port_id)[0]
             portfolio_serializer = PortfolioSerializer(portfolio_f, data=portfolio_data)
         else:
             portfolio_serializer = PortfolioSerializer(data=portfolio_data)
@@ -100,5 +110,60 @@ def ImageAddViewSet(request):
         if image_id is not None:
             entry = image.filter(id=image_id)
             count = entry.delete()
-            return JsonResponse({'message': '{} Images were deleted successfully!'.format(count[0])}, status=status.HTTP_204_NO_CONTENT)
+            return JsonResponse({'message': '{} Images were deleted successfully!'.format(count[0])},
+                                status=status.HTTP_204_NO_CONTENT)
         return JsonResponse({'message': 'Invalid Image ID!'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["POST"])
+def EditView(request):
+    def get_random_string(length):
+        letters = string.ascii_lowercase
+        result_str = ''.join(random.choice(letters) for i in range(length))
+        return result_str
+
+    if request.method == "POST":
+        image_data = JSONParser().parse(request)
+        url = image_data['url']
+        extension = url.split(".")[-1]
+        user_id = image_data['user_id']
+        edits = image_data['edits']
+        pth = os.path.dirname(settings.MEDIA_ROOT)
+        image = requests.get(url, allow_redirects=True)
+
+        while True:
+            local_url = get_random_string(20)
+            new_file = (local_url + "." + extension)
+            pth = os.path.join(pth, (local_url + "." + extension))
+            if not os.path.exists(pth):
+                break
+        new_url = request.build_absolute_uri(settings.MEDIA_URL + new_file)
+        im = PImage.open(BytesIO(image.content))
+        if 'crop' in edits.keys():
+            crop = edits['crop']
+            box = (crop[0], crop[1], crop[2], crop[3])
+            mod_image = im.crop(box)
+        if 'resize' in edits.keys():
+            resize = (edits['resize'][0], edits['resize'][1])
+            mod_image = im.resize(resize)
+
+        mod_image.save(pth)
+        # save modified image to database
+        payload = {"url": new_url}#, "mods": edits}
+        image_r = requests.post("http://127.0.0.1:8000/image/", json=payload)
+        image_id = json.loads(image_r.content)['id']
+        payload = {"user": user_id, "photo": image_id}
+        port_r = requests.post("http://127.0.0.1:8000/portfolio/", json=payload)
+        print(port_r.content)
+
+        return JsonResponse({"message": "Created"}, status=status.HTTP_201_CREATED)
+
+
+def download(request, path):
+    file_path = os.path.join(settings.MEDIA_ROOT, path)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read())
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Http404
